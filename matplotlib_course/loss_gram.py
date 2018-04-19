d852241779966adfd7fa6ec12122fc19045a6b79
zhra46 <241465144@qq.com>
2018-04-19 11:51:50 +0800

    first

--- matplotlib_course/loss_gram.py
+++ matplotlib_course/loss_gram.py
@@ -28,23 +28,19 @@ theta2_space = np.linspace(theta2-3,theta2+3,10)
 result2_ = np.array([loss_function(X,theta1,i,y) for i in theta2_space])
 result1_ = np.array([loss_function(X,i,theta2,y) for i in theta1_space])
 fig = plt.figure()
-plt.subplot(2,1,1)
+plt.subplot(2,2,1)
 plt.plot(theta2_space,result2_)
-plt.subplot(2,1,2)
+plt.subplot(2,2,2)
 plt.plot(theta1_space,result1_)
 
 # plt.show()
 
-theta1_space = np.linspace(theta1-3,theta1+3,500)
-theta2_space = np.linspace(theta2-3,theta2+3,500)
+theta1_space = np.linspace(theta1-3,theta1+3,9)
+theta2_space = np.linspace(theta2-3,theta2+3,9)
 theta1_grid,theta2_grid = np.meshgrid(theta1_space,theta2_space)
-result_ = np.zeros((500,500))
-
-for i in range(len(theta1_space)):
-    for j in range(len(theta2_space)):
-        result_[i,j]=loss_function(X,theta1_space[i],theta2_space[j],y)
-
-print(result_)
-# plt.contourf(theta1_space,theta2_space,result_,10,alpha=0.6,cmap=plt.cm.hot)
-plt.contour3D(theta1_space,theta2_space,result_,10,alpha=0.6,cmap=plt.cm.hot)
+high_grid = loss_function(X,theta1_grid,theta2_grid,y)
+print(high_grid)
+plt.subplot(2,2,3)
+plt.contourf(theta1_grid,theta2_grid,high_grid,10,alpha=0.6,cmap=plt.cm.hot)
+# plt.contour3D(theta1_space,theta2_space,result_,10,alpha=0.6,cmap=plt.cm.hot)
 plt.show()
